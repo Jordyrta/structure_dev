a9a3fb499bceb0eff4cfddcdbbeb1118570fd4a8
jordyrta <jordygael34@yahoo.fr>
2022-12-01 12:24:29 +0000

    ajout_Picsum

---
+++ 5_Exercices_En_Cours_Formation/picsumFetch/picsum.js
@@ -0,0 +1,59 @@
+let mainSelector = document.querySelector("main");
+let bodySelector = document.querySelector("body");
+let sectionCreation;
+let asideCreation;
+let textCreation;
+let buttonCreation;
+let articleCreation;
+let imgCreation;
+
+fetch("https://picsum.photos/v2/list?page=1&limit=40")
+  .then((response) => {
+    return response.json();
+  })
+  .then((photos) => {
+    photos.forEach((photo) => {
+      createElements();
+      fillElements(photo.download_url, photo.author, photo.url);
+      appendElements();
+      buttonCreation.addEventListener("click", function(){
+        document.location.href=photo.url;
+      })
+    });
+  });
+
+function createElements() {
+  sectionCreation = document.createElement("section");
+  asideCreation = document.createElement("aside")
+  textCreation = document.createElement("p");
+  buttonCreation = document.createElement("button");
+  imgCreation = document.createElement("img");
+  articleCreation = document.createElement("article")
+}
+
+function fillElements(photoUrl, photoAuthor) {
+  console.log("hello");
+  textCreation.textContent = photoAuthor;
+  buttonCreation.textContent = "Visit";
+  imgCreation.src = photoUrl;
+}
+
+function appendElements() {
+  mainSelector.append(sectionCreation);
+  sectionCreation.append(asideCreation);
+  asideCreation.append(textCreation);
+  asideCreation.append(buttonCreation);
+  sectionCreation.append(articleCreation);
+  articleCreation.append(imgCreation);
+}
+
+
+/*
+<section>
+  <aside>
+    <p>Alejandro Escamilla</p>
+    <button>Visit</button>
+  </aside>
+  <img src="https://picsum.photos/id/0/5000/3333" alt="">
+</section>
+*/
